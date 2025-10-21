ec315ab09e8d44f941f2343c0660ec536d4f9012
xiaoR <2471385107@qq.com>
2025-10-21 20:47:47 +0800

    feat(java): 添加守护线程和同步卖票示例
    - 实现守护线程示例，展示线程守护机制
    - 创建卖票资源类，包含同步方法保证线程安全
    - 添加三个线程模拟并发卖票场景
    - 演示多线程环境下共享资源的安全访问
    - 完善类注释信息，增强代码可读性

--- src/main/java/com/xiaoR/deamon_thread.java
+++ src/main/java/com/xiaoR/deamon_thread.java
@@ -4,7 +4,21 @@ package com.xiaoR;
  * @author xiaoR
  * @version 1.0
  * @date 2025/10/20
- * @description 
+ * @description
  */
 public class deamon_thread {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "::" + Thread.currentThread().isDaemon());
+            while (true) {
+
+            }
+        }, "t1");
+
+        // 设置为守护线程
+        t1.setDaemon(true);
+        t1.start();
+
+        System.out.println(Thread.currentThread().getName() + "over~");
+    }
 }
--- src/main/java/com/xiaoR/sync/SaleTicket.java
+++ src/main/java/com/xiaoR/sync/SaleTicket.java
@@ -4,7 +4,49 @@ package com.xiaoR.sync;
  * @author xiaoR
  * @version 1.0
  * @date 2025/10/21
- * @description 
+ * @description
  */
+
+// 1. 创建资源类，定义属性和操作方法
+class Ticket {
+    private int number = 30;
+    public synchronized void sale() {
+        if (number > 0) {
+            System.out.println(Thread.currentThread().getName() + ": 卖出第" + (number--) + "张票，剩余：" + number);
+        }
+    }
+}
+
 public class SaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+
+        // 2. 创建三个线程，调用卖票方法
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "t1").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "t2").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        }, "t3").start();
+    }
 }
