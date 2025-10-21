483d9ad0729a7ff0af54b9ca3063c5d2f13c939b
xiaoR <2471385107@qq.com>
2025-10-21 20:47:17 +0800

    init commit

---
+++ src/main/java/com/xiaoR/deamon_thread.java
@@ -0,0 +1,10 @@
+package com.xiaoR;
+
+/**
+ * @author xiaoR
+ * @version 1.0
+ * @date 2025/10/20
+ * @description 
+ */
+public class deamon_thread {
+}
---
+++ src/main/java/com/xiaoR/sync/SaleTicket.java
@@ -0,0 +1,10 @@
+package com.xiaoR.sync;
+
+/**
+ * @author xiaoR
+ * @version 1.0
+ * @date 2025/10/21
+ * @description 
+ */
+public class SaleTicket {
+}
